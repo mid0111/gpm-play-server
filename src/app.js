ae5dfad3ecb06955cc2632bd2ba06404c1094be7
mid0111 <m.tajima0111@gmail.com>
2017-02-12 16:15:02 +0900

    Remove sass engine.

--- src/app.js
+++ src/app.js
@@ -5,8 +5,6 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const saasEngine = require('node-sass-middleware');
-
 const routes = require('./routes');
 const Logger = require('./libs/Logger');
 
@@ -22,12 +20,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(saasEngine({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true,
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
