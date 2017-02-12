5d47957f5181bfa17a52b5a08d8cf82a18858daa
mid0111 <m.tajima0111@gmail.com>
2017-02-12 09:57:03 +0900

    Create class of music player.

---
+++ libs/MusicPlayer.js
@@ -0,0 +1,56 @@
+var fs = require('fs-extra');
+var path = require('path');
+var request = require('request');
+var player = require('play-sound')(opts = {});
+
+var PlayList = require('./PlayList');
+
+var tmpFolder = path.join(__dirname, '../tmp');
+var tmpFilePath = path.join(tmpFolder, 'playing.mp3');
+
+class MusicPlayer {
+
+  constructor(playList) {
+    this.playList = playList;
+  }
+
+  static init() {
+    // Create download directory when it does not exist.
+    fs.mkdirsSync(tmpFolder);
+
+    // Create instance
+    return PlayList.create()
+      .then((playList) => new MusicPlayer(playList));
+  }
+
+  play() {
+    return this.playList.select()
+      .then(this._downloadMusicFile)
+      .then(this._play);
+  }
+
+  _downloadMusicFile(streamUrl) {
+    return new Promise((resolve) => {
+      var writeStream = fs.createWriteStream(tmpFilePath);
+      writeStream.on('finish', () => {
+        return resolve(tmpFilePath);
+      });
+      request(streamUrl).pipe(writeStream);
+    });
+  }
+
+  _play() {
+    return new Promise((resolve, reject) => {
+      console.log('im playing...');
+
+      player.play(tmpFilePath, (err)=> {
+        if(err) reject(err);
+
+        console.log('play done, switching to next one ...');
+        return resolve();
+      });
+    });
+  }
+}
+
+module.exports = MusicPlayer;
---
+++ libs/PlayList.js
@@ -0,0 +1,68 @@
+var PlayMusic = require('playmusic');
+var request = require('request');
+var fs = require('fs-extra');
+var path = require('path');
+
+var secret = require('../config/secret.json');
+var email = secret.email;
+var password = secret.password;
+
+class PlayList {
+
+  constructor(pm, songs) {
+    this.pm = pm;
+    this.songs = songs;
+  }
+
+  static create() {
+    var pm = new PlayMusic();
+    return this.getAllTracks(pm)
+      .then((songs) => new PlayList(pm, songs));
+  }
+
+  static getAllTracks(pm) {
+    var playList = [];
+
+    return new Promise((resolve) => {
+      console.log('Get all tracks.');
+
+      pm.init({
+        email: email,
+        password: password
+      }, (err, res) => {
+        if(err) throw err;
+
+        pm.getAllTracks((err, library) => {
+          if(err) throw err;
+
+          console.log('all tracks length:', library.data.items.length);
+          resolve(library.data.items);
+        });
+      });
+    });
+  }
+
+  select() {
+    return new Promise((resolve) => {
+      console.log('Select songs.');
+
+      var randomIndex = Math.floor(Math.random() * this.songs.length);
+      var song = this.songs[randomIndex];
+
+      console.log('song:', `${song.artist} ${song.title}`);
+
+      resolve(this.getStreamUrl(song));
+    });
+  }
+
+  getStreamUrl(song) {
+    return new Promise((resolve) => {
+      this.pm.getStreamUrl(song.id, (err, streamUrl) => {
+        if(err) throw err;
+        resolve(streamUrl);
+      });
+    });
+  }
+}
+
+module.exports = PlayList;
